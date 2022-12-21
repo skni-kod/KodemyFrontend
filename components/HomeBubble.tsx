bd7a6037f8af52a299a9e6af2a480d4a117df680
Maksymilian Dendura <maksd0410@gmail.com>
2022-12-21 19:34:34 +0100

    Dokończenie strony głównej

--- components/HomeBubble.tsx
+++ components/HomeBubble.tsx
@@ -17,9 +17,9 @@ const HomeBubble = ({ children, className }: Props) => {
     <div
       className={
         active
-          ? "cursor-pointer relative p-2 rounded-full min-h-[80px] border-sky-500 border-2 font-semibold aspect-square text-center flex items-center justify-center text-xl text-sky-500 shadow-lg " +
+          ? "cursor-pointer relative p-2 rounded-full min-h-[80px] border-sky-500 border-2 font-semibold aspect-square text-center flex items-center justify-center text-xl text-sky-500 shadow-lg bg-white " +
             className
-          : "cursor-pointer relative p-2 rounded-full min-h-[80px] border-black border-2 font-semibold aspect-square text-center flex items-center justify-center text-xl " +
+          : "cursor-pointer relative p-2 rounded-full min-h-[80px] border-black border-2 font-semibold aspect-square text-center flex items-center justify-center text-xl bg-white " +
             className
       }
       onClick={handleClick}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import Head from "next/head";
+import { useState } from "react";
 import HomeBubble from "../components/HomeBubble";
 
 export default function Home() {
+  const [stage, setStage] = useState(1);
+
   return (
     <>
       <Head>
@@ -12,30 +15,93 @@ export default function Home() {
         />
       </Head>
 
-      <div className="flex flex-wrap gap-2 justify-center items-center mx-auto max-w-xl">
-        <HomeBubble className="basis-6/12">
-          <>
-            Języki <br />
-            programowania
-          </>
-        </HomeBubble>
-        <HomeBubble className="basis-5/12 relative top-16">
-          <>GameDev</>
-        </HomeBubble>
-        <HomeBubble className="basis-5/12">
-          <>
-            Elektroniki <br /> Retro
-          </>
-        </HomeBubble>
-        <HomeBubble className="basis-5/12 relative top-14 left-4">
-          <>Inne</>
-        </HomeBubble>
-        <HomeBubble className="basis-5/12 relative right-16">
-          <>
-            Aplikacje <br /> webowe
-          </>
-        </HomeBubble>
+      <div className="px-7">
+        <div className="flex justify-center mt-8">
+          <div className="flex items-center gap-4" bg-white>
+            <div className="rounded-full border-sky-500 text-sky-500 shadow-lg border-2 h-10 w-10 flex items-center justify-center font-semibold bg-white">
+              1
+            </div>
+            <div className="h-[3px] w-6 bg-sky-500"></div>
+          </div>
+          <div className="flex items-center gap-4">
+            <div
+              className={
+                stage != 2 ? "h-[3px] w-6 bg-black" : "h-[3px] w-6 bg-sky-500"
+              }
+            ></div>
+            <div
+              className={
+                stage == 2
+                  ? "rounded-full border-sky-500 text-sky-500 shadow-lg border-2 h-10 w-10 flex items-center justify-center font-semibold bg-white"
+                  : "rounded-full border-black border-2 h-10 w-10 flex items-center justify-center font-semibold bg-white"
+              }
+            >
+              2
+            </div>
+          </div>
+        </div>
+        <h1 className="text-2xl font-bold text-center mt-4 mb-8">
+          {stage == 1 ? "Wybierz sekcję" : "Wybierz kategorię"}
+        </h1>
+
+        {stage == 1 ? (
+          <div className="flex flex-wrap gap-2 justify-center items-center mx-auto max-w-xl">
+            <HomeBubble className="basis-6/12">
+              <>
+                Języki <br />
+                programowania
+              </>
+            </HomeBubble>
+            <HomeBubble className="basis-5/12 relative top-16">
+              <>GameDev</>
+            </HomeBubble>
+            <HomeBubble className="basis-5/12 top-2">
+              <>
+                Elektroniki <br /> Retro
+              </>
+            </HomeBubble>
+            <HomeBubble className="basis-5/12 relative top-14 left-4">
+              <>Inne</>
+            </HomeBubble>
+            <HomeBubble className="basis-5/12 relative right-12 top-4">
+              <>
+                Aplikacje <br /> webowe
+              </>
+            </HomeBubble>{" "}
+          </div>
+        ) : (
+          <div className="flex flex-wrap gap-4 justify-center items-center mx-auto max-w-xl">
+            <HomeBubble className="basis-6/12">
+              <>
+                GameDev
+                <br />
+                Ogólny
+              </>
+            </HomeBubble>
+            <HomeBubble className="basis-5/12 relative bottom-4">
+              <>Unity</>
+            </HomeBubble>
+            <HomeBubble className="basis-5/12 relative">
+              <>Unreal</>
+            </HomeBubble>
+            <HomeBubble className="basis-5/12 relative bottom-6">
+              <>Grafika 2D</>
+            </HomeBubble>
+            <HomeBubble className="basis-5/12 relative">
+              <>Godot</>
+            </HomeBubble>
+            <HomeBubble className="basis-5/12 relative bottom-2">
+              <>Grafika 3D</>
+            </HomeBubble>
+          </div>
+        )}
       </div>
+      <button
+        className="bg-sky-500 text-white font-semibold mx-auto block py-3 px-6 rounded-2xl mt-16"
+        onClick={() => setStage(2)}
+      >
+        Następne &gt;
+      </button>
     </>
   );
 }
