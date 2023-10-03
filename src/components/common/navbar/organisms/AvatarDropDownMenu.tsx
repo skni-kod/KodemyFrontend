5a8c29002b38235c300dd3d455af747ff4f819dc
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-10-03 22:57:54 +0200

    feat #25 add text to AvatarDropDownMenu

--- src/components/common/navbar/organisms/AvatarDropDownMenu.tsx
+++ src/components/common/navbar/organisms/AvatarDropDownMenu.tsx
@@ -1,7 +1,18 @@
 const AvatarDropDownMenu = () => {
 	return (
-		<div className="bg-gray-200 h-[200px] w-[150px] absolute top-[70px] right-[0px]">
-			<h1 className=" text-black text-[20px] p-1">dasd</h1>
+		<div className="bg-white h-[auto] w-[160px] absolute top-[70px] right-[0px] shadow-md rounded-lg p-2">
+			<div className="text-[#666] text-[12px] p-1">
+				<a>
+					<button>Ustawienia konta</button>
+				</a>
+				<div className="pt-1">Twoje materiały:</div>
+				<button className="pl-3 pt-1">Zatwierdzone</button>
+				<button className="pl-3 pt-1">Nie zatwierdzone</button>
+				<button className="pl-3 pt-1">Ulubione</button>
+			</div>
+			<div className="text-black font-semibold text-[12px] p-1">
+				<button>Wyloguj się</button>
+			</div>
 		</div>
 	);
 };
