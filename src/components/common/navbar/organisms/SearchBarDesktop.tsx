ece5ef6fb2c54c16e48e6862d6f858c4f623e016
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-10-03 23:34:27 +0200

    feat #25  preparing SearchBar for phones

--- src/components/common/navbar/organisms/SearchBarDesktop.tsx
+++ src/components/common/navbar/organisms/SearchBarDesktop.tsx
@@ -1,13 +1,13 @@
 import { BiSearch } from 'react-icons/bi';
 import { PiMicrophoneThin } from 'react-icons/pi';
 
-const SearchBar = () => {
+const SearchBarDesktop = () => {
 	const IconClassNames = 'text-gray-400 text-2xl h-[25.5px] w-[25.5px]';
 	return (
 		<div className="h-[50px] w-[auto] flex items-center border border-gray-300 rounded-full mx-3">
 			<BiSearch className={`${IconClassNames} ml-[5px]`} />
 			<input
-				className="h-[47px] w-[450px] text-[18px] outline-none text-[#666] px-[5px]"
+				className="h-[47px] w-[30vw] text-[18px] outline-none text-[#666] px-[5px]"
 				type="text"
 				placeholder="Szybkie wyszukiwanie"
 			/>
@@ -18,4 +18,4 @@ const SearchBar = () => {
 	);
 };
 
-export default SearchBar;
+export default SearchBarDesktop;
