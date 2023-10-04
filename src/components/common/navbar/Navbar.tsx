699a60e0a958267b1081f36616b6a954eaa9feb6
marcinbator <marcinbator.ofc@gmail.com>
2023-10-04 07:56:17 +0200

    refactor: #16 revert navbar changes

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ import UserBar from '@/components/common/navbar/organisms/UserBar';
 
 const Navbar = () => {
 	return (
-		<nav className="fixed top-0 w-full left-0 bg-white flex items-center justify-between px-7 py-3.5 z-20 shadow-md">
+		<nav className="fixed top-0 w-full left-0 bg-white flex items-center justify-between px-7 py-3.5 z-10 shadow-md">
 			<div className="flex items-center gap-x-[10px] text-2xl font-semibold text-blue-500">
 				<TfiMenu className="hidden" />
 				<Logo width={155} height={100} />
