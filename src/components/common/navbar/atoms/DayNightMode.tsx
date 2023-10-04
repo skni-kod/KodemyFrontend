2f89a0c0c9cd2922043b265fe404685f8dcfb886
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-10-04 16:37:39 +0200

    Revert "fix #27 DayNightMode"
    This reverts commit f79fcfa099a9de68bc92cbcd7a185e4a39c1df19.

--- src/components/common/navbar/atoms/DayNightMode.tsx
+++ src/components/common/navbar/atoms/DayNightMode.tsx
@@ -1,44 +1,29 @@
 import { BiSun } from 'react-icons/bi';
 import { HiOutlineMoon } from 'react-icons/hi2';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const DayNightMode = () => {
 	const IconClassNames =
 		'cursor-pointer text-gray-400 text-2xl h-[25.5px] w-[25.5px]';
 
-	const [dayNightSwitch, setDayNightSwitch] = useState(false);
-
-	useEffect(() => {
-		const currentTheme = localStorage.getItem('theme');
-		if (currentTheme === 'dark') {
-			setDayNightSwitch(true);
-		}
-	}, []);
-
+	const [dayNightSwitch, setdayNightSwitch] = useState(false);
 	const toggleDayNightSwitch = () => {
-		setDayNightSwitch(!dayNightSwitch);
+		setdayNightSwitch(!dayNightSwitch);
 	};
-
 	const setDarkMode = () => {
 		console.log('Ustawiono ciemny motyw');
 		document.querySelector('body').setAttribute('data-theme', 'dark');
-		localStorage.setItem('theme', 'dark');
 	};
 
 	const setLightMode = () => {
 		console.log('Ustawiono jasny motyw');
 		document.querySelector('body').setAttribute('data-theme', 'light');
-		localStorage.setItem('theme', 'light');
 	};
 
-	const toggleTheme = () => {
-		if (dayNightSwitch) {
-			setLightMode();
-		} else {
-			setDarkMode();
-		}
+	const toggleTheme = (e: any) => {
+		if (e.target.checked) setDarkMode();
+		else setLightMode();
 	};
-
 	return (
 		<div onClick={toggleDayNightSwitch} onChange={toggleTheme}>
 			{dayNightSwitch ? (
