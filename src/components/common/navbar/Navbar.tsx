9fbf67c5ecb84044424e6469831f69315ff97946
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-10-07 16:16:32 +0200

    feat: #35 responsive SearchBar and UserBar

--- src/components/common/navbar/Navbar.tsx
+++ src/components/common/navbar/Navbar.tsx
@@ -7,16 +7,17 @@ import Link from 'next/link';
 import { pageHomeRoute } from '@/pages';
 
 const Navbar = () => {
+	const IconClassNames = 'text-grey2white text-2xl h-[25.5px] w-[25.5px]';
 	return (
-		<nav className="fixed top-0 w-full left-0 bg-white2darkgrey flex items-center justify-between px-7 py-3.5 z-20 shadow-md">
+		<nav className="fixed top-0 w-full left-0 bg-white2darkgrey flex items-center justify-between sm:px-7 sm:py-3.5 px-[1vw] py-[1vh] z-20 shadow-md">
 			<div className="flex items-center gap-x-[10px] text-2xl font-semibold text-blue-500">
-				<TfiMenu className="hidden" />
 				<Link href={pageHomeRoute()}>
 					<Logo width={155} height={100} theme={'light'} />
 				</Link>
 			</div>
 			<SearchBarDesktop />
 			<UserBar />
+			<TfiMenu className={`${IconClassNames} block md:hidden cursor-pointer`} />
 		</nav>
 	);
 };
--- src/components/common/navbar/organisms/SearchBarDesktop.tsx
+++ src/components/common/navbar/organisms/SearchBarDesktop.tsx
@@ -2,12 +2,13 @@ import { BiSearch } from 'react-icons/bi';
 import { PiMicrophoneThin } from 'react-icons/pi';
 
 const SearchBarDesktop = () => {
-	const IconClassNames = 'text-grey2white text-2xl h-[25.5px] w-[25.5px]';
+	const IconClassNames =
+		'text-grey2white text-2xl h-[5vw] w-[5vw] sm:h-[25.5px] sm:w-[25.5px]';
 	return (
-		<div className="h-[50px] w-[auto] flex items-center border border-grey2white rounded-full mx-3 bg-white2darkgrey">
+		<div className="h-[auto] sm:h-[50px] w-[auto] flex items-center border border-grey2white rounded-full mx-3 bg-white2darkgrey">
 			<BiSearch className={`${IconClassNames} ml-[5px]`} />
 			<input
-				className="h-[47px] w-[20vw] text-[18px] outline-none bg-white2darkgrey text-black2white px-[5px]"
+				className="h-[auto] py-[1vh] w-[33vw] sm:h-[47px] sm:w-[20vw] text-[2.8vw] sm:text-[18px] sm:min-w-[205px] outline-none bg-white2darkgrey text-black2white px-[5px]"
 				type="text"
 				placeholder="Szybkie wyszukiwanie"
 			/>
