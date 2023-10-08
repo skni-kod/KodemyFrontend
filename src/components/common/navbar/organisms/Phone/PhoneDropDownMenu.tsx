c7048fdf23c6da5e9f580e7a8f91b81e189933ed
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-10-08 11:05:33 +0200

    feat: #44 working PhoneMenu

--- src/components/common/navbar/organisms/Phone/PhoneDropDownMenu.tsx
+++ src/components/common/navbar/organisms/Phone/PhoneDropDownMenu.tsx
@@ -8,7 +8,6 @@ import PhoneDropDownMenuItem from '../../molecules/PhoneDropDownMenuItem';
 const PhoneDropDownMenu = () => {
 	const [sections, setSections] = useState<Section[]>([]);
 	const { getSections } = useSectionService();
-	const [isExpandMenu, setIsExpandMenu] = useState(false);
 	const [expandedItemId, setExpandedItemId] = useState<number | null>(null);
 
 	useEffect(() => {
@@ -17,40 +16,27 @@ const PhoneDropDownMenu = () => {
 		})();
 	}, [getSections]);
 
-	const handleExpand = (mouseEntered: boolean) => setIsExpandMenu(mouseEntered);
 	return (
-		<div className=" px-[8vw] bg-white2darkgrey h-[auto] w-[100%] absolute top-[63px] right-[0px] shadow-md">
-			<h1 className=" text-black2white">Miejsce 1</h1>
-			<h1 className=" text-black2white">Miejsce 2</h1>
-			<h1 className=" text-black2white">Miejsce 3</h1>
-			<h1 className=" text-black2white">Miejsce 4</h1>
-			<h1 className=" text-black2white">Miejsce 5</h1>
+		<div className="px-[8vw] bg-white2darkgrey h-auto w-[100%] absolute top-[63px] right-[0px] shadow-md">
 			<div
-				onMouseEnter={() => handleExpand(true)}
-				onMouseLeave={() => handleExpand(false)}
 				className={clsx(
-					'md:hidden z-10 fixed top-0 left-0 h-full pt-24 pb-5 overflow-y-auto bg-white2darkgrey text-black2white shadow-md transition-width duration-500 ease-linear',
-					isExpandMenu
-						? 'w-72 px-5 bg-gradient-radial'
-						: 'w-18 px-5 bg-gradient-conic',
+					'md:hidden w-[100%] bg-white2darkgrey text-black2white px-[2vw]',
 				)}
+				style={{ display: 'flex', flexDirection: 'column' }}
 			>
-				<div className="relative">
-					<ul className="p-0 list-none">
-						{sections &&
-							sections.map((section, index) => (
-								<PhoneDropDownMenuItem
-									key={index}
-									section={section}
-									expandedItemId={expandedItemId}
-									setExpandedItemId={setExpandedItemId}
-								/>
-							))}
-					</ul>
-				</div>
+				<ul className="p-0 list-none">
+					{sections &&
+						sections.map((section, index) => (
+							<PhoneDropDownMenuItem
+								key={index}
+								section={section}
+								expandedItemId={expandedItemId}
+								setExpandedItemId={setExpandedItemId}
+							/>
+						))}
+				</ul>
+				<UserBarPhone />
 			</div>
-
-			<UserBarPhone />
 		</div>
 	);
 };
