f9ba3bde45556b89fd343208f158681c40840292
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2023-10-09 23:31:58 +0200

    feat: #55 restrict login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,6 +2,7 @@ import Page from '@/components/common/Page';
 import Container from '@/components/common/Container';
 import LoginPage from '@/components/login/login';
 import Route from '@/utils/route';
+import { useCheckLoginStatus } from '@/components/login/CheckLoginStatus';
 
 export const pageLoginRoute = (): Route => {
 	return {
@@ -10,10 +11,21 @@ export const pageLoginRoute = (): Route => {
 };
 
 const Index = () => {
+	const isLoggedIn = useCheckLoginStatus();
 	return (
 		<Page title="Panel logowania" description="Panel logowania">
 			<Container>
-				<LoginPage />
+				{isLoggedIn === false ? (
+					<LoginPage />
+				) : isLoggedIn === true ? (
+					<div className="h-[auto] flex items-center justify-center">
+						<h1 className="text-black2white text-[20px]">Już jesteś zalogowany</h1>
+					</div>
+				) : (
+					<div className="h-[auto] flex items-center justify-center">
+						<h1 className="text-black2white text-[20px]">Już jesteś zalogowany</h1>
+					</div>
+				)}
 			</Container>
 		</Page>
 	);
