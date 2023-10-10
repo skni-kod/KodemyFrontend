fff761698c69ebee104acada983e50650097bdb5
Krystian Kiełbasa <krystian.kielbasa@hotmail.com>
2023-10-10 18:40:33 +0200

    refactor: #57 theme init/reload changes

---
+++ src/hooks/useWindowWidth.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+const useWindowWidth = () => {
+	const [windowWidth, setWindowWidth] = useState<number>(
+		typeof window !== 'undefined' ? window.innerWidth : 0,
+	);
+
+	const handleResize = () => {
+		setWindowWidth(window.innerWidth);
+	};
+
+	useEffect(() => {
+		window.addEventListener('resize', handleResize);
+		return () => {
+			window.removeEventListener('resize', handleResize);
+		};
+	}, []);
+
+	return windowWidth;
+};
+
+export default useWindowWidth;
