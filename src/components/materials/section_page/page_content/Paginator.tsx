0695c5621b07f1a9b0d7b909225c277c0cac5e2a
Wiktor-Mazur <zegarekpl.gaming@gmail.com>
2024-04-09 11:22:41 +0200

    feat: #95 Working Pagination

--- src/components/materials/section_page/page_content/Paginator.tsx
+++ src/components/materials/section_page/page_content/Paginator.tsx
@@ -22,34 +22,26 @@ const Paginator: React.FC<PaginatorProps> = ({ page, total }) => {
 		router.push(router.pathname + `?${newParams}`);
 	};
 
+	const paginationVisiblePages = 3;
 	const paginationRange = [];
-	const visiblePages = 5;
-	let startPage = page - 2;
-	let endPage = page + 2;
 
-	if (startPage < 1) {
-		startPage = 1;
-		endPage = Math.min(visiblePages, total);
-	}
+	let startPage = Math.max(1, page - Math.floor(paginationVisiblePages / 2));
+	let endPage = Math.min(total, startPage + paginationVisiblePages - 1);
 
-	if (endPage > total) {
-		endPage = total;
-		startPage = Math.max(1, endPage - visiblePages + 1);
+	if (endPage - startPage + 1 < paginationVisiblePages) {
+		startPage = Math.max(1, endPage - paginationVisiblePages + 1);
 	}
 
 	for (let i = startPage; i <= endPage; i++) {
 		paginationRange.push(i);
 	}
 
-	const shouldDisplayFirstPage = startPage > 1;
-	const shouldDisplayLastPage = endPage < total;
-
 	return (
 		<div className="flex items-center gap-6 mx-auto">
-			{page + 1 > 1 && (
+			{page > 1 && (
 				<PaginatorBtn
 					className="flex items-center"
-					onClick={() => handlePageChange(page + 1 - 1)}
+					onClick={() => handlePageChange(page - 1)}
 					selected={true}
 				>
 					<FaArrowLeft />
@@ -57,36 +49,36 @@ const Paginator: React.FC<PaginatorProps> = ({ page, total }) => {
 				</PaginatorBtn>
 			)}
 			<div className="flex items-center gap-2">
-				{shouldDisplayFirstPage && (
+				{startPage > 1 && (
 					<>
-						<PaginatorBtn onClick={() => handlePageChange(1)} selected={page + 1 === 1}>
+						<PaginatorBtn onClick={() => handlePageChange(1)} selected={page === 1}>
 							1
 						</PaginatorBtn>
-						<HiOutlineDotsHorizontal />
+						{startPage > 2 && <HiOutlineDotsHorizontal />}
 					</>
 				)}
 				{paginationRange.map((pageNum) => (
 					<PaginatorBtn
 						key={pageNum}
 						onClick={() => handlePageChange(pageNum)}
-						selected={page + 1 === pageNum}
+						selected={page === pageNum}
 					>
 						{pageNum}
 					</PaginatorBtn>
 				))}
-				{shouldDisplayLastPage && (
+				{endPage < total && (
 					<>
-						<HiOutlineDotsHorizontal />
-						<PaginatorBtn onClick={() => handlePageChange(total)} selected={page + 1 === total}>
+						{total - endPage > 1 && <HiOutlineDotsHorizontal />}
+						<PaginatorBtn onClick={() => handlePageChange(total)} selected={page === total}>
 							{total}
 						</PaginatorBtn>
 					</>
 				)}
 			</div>
-			{page + 1 < total && (
+			{page < total && (
 				<PaginatorBtn
 					className="flex items-center"
-					onClick={() => handlePageChange(page + 1 + 1)}
+					onClick={() => handlePageChange(page + 1)}
 					selected={true}
 				>
 					Następna&nbsp;
