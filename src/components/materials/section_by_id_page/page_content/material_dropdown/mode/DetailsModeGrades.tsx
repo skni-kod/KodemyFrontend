3e58d8dc61b6bc8102ef37c28a68f82cef004569
Wiktor Mazur <zegarekpl.gaming@gmail.com>
2024-11-11 13:54:14 +0100

    fix: #114 grade changes
    * fix: #114 delete unused packages
    
    * feat: #114 update Readme
    
    * fix: #114 fix update Readme
    
    * feat: #114 update packages
    
    * fix: #114 fix update Readme v2
    
    * revert: #114 revert bad commits
    
    * feat: #114 some small changes
    
    * feat: #114 add grading materials
    
    * fix: #114 fix material loading

--- src/components/materials/section_by_id_page/page_content/material_dropdown/mode/DetailsModeGrades.tsx
+++ src/components/materials/section_by_id_page/page_content/material_dropdown/mode/DetailsModeGrades.tsx
@@ -16,41 +16,84 @@ export default function DetailsModeGrades({ id }: { id: number }) {
 	if (status === Status.PENDING) return <Loading scale="small" />;
 	if (status === Status.ERROR || !material) return <Error />;
 
+	function between(value: number, min: number, max: number) {
+		return value > min && value <= max;
+	}
+
+	const getRatingDescription = (grade: number) => {
+		switch (true) {
+			case grade === 0:
+				return 'Brak ocen';
+			case between(grade, 0, 1.5):
+				return 'Bardzo słaby';
+			case between(grade, 1.5, 2.5):
+				return 'Słaby';
+			case between(grade, 2.5, 3.5):
+				return 'Średni';
+			case between(grade, 3.5, 4.5):
+				return 'Dobry';
+			case between(grade, 4.5, 5.0):
+				return 'Rewelacyjny';
+			default:
+				return 'Nieznana ocena';
+		}
+	};
+
 	return (
-		<div className="p-7">
-			<div className="w-full">
-				<h2 className="text-lg font-semibold">Oceny</h2>
-				<p className="pt-2">Średnia ocen: {material.averageGrade.toFixed(2)}</p>
+		<div className="w-full p-7">
+			<h2 className="text-lg font-semibold">Opinie o produkcie</h2>
+			<div className="grid grid-cols-1 items-center md:grid-cols-[1fr_2fr]">
+				<div className="flex w-full flex-col items-center">
+					<h3 className="flex items-center text-lg font-bold">{getRatingDescription(material.averageGrade)}</h3>
+					<div className="flex items-center">
+						<p className="text-2xl font-bold">{material.averageGrade.toFixed(2)}</p>
+						<p className="text-xl">{'/ 5'}</p>
+					</div>
+				</div>
+				<table className="h-full w-full border-black md:border-l-2">
+					<tbody>
+						{material.gradeStats
+							.slice()
+							.reverse()
+							.map((stat, index) => {
+								const totalRatings = material.gradeStats.reduce((acc, curr) => acc + curr, 0);
+								const percentage = (stat / totalRatings) * 100;
+
+								return (
+									<tr key={index}>
+										<td className="pl-4 text-center">
+											<h1>{material.gradeStats.length - index}</h1>
+										</td>
+										<td className="flex px-2">
+											<Rating
+												initialValue={material.gradeStats.length - index}
+												SVGstyle={{ display: 'inline' }}
+												size={16}
+												allowFraction
+												readonly
+											/>
+										</td>
+										<td className="w-full px-4 py-2">
+											<div className="relative h-4 w-full rounded bg-gray-200">
+												<div
+													className="absolute left-0 top-0 h-full rounded bg-grade"
+													style={{ width: `${percentage}%` }}
+												/>
+											</div>
+										</td>
+										<td className="px-2 text-left">
+											<h1>{stat}</h1>
+										</td>
+									</tr>
+								);
+							})}
+					</tbody>
+				</table>
 			</div>
-			<table>
-				<tbody>
-					{material.gradeStats
-						.slice()
-						.reverse()
-						.map((stat, index) => {
-							return (
-								<tr key={index}>
-									<td className="flex items-center px-4 py-2">
-										<Rating
-											initialValue={material.gradeStats.length - index}
-											SVGstyle={{ display: 'inline' }}
-											size={16}
-											allowFraction
-											readonly
-										/>
-									</td>
-									<td className="px-4 py-2">
-										<h1>{stat}</h1>
-									</td>
-								</tr>
-							);
-						})}
-				</tbody>
-			</table>
 			<div className="flex w-full justify-end pt-4">
 				<Link
 					href={material.link}
-					className="flex·h-9·items-center·gap-1·rounded-xl·bg-grade·px-4·text-lg·font-semibold·text-gradeText"
+					className="flex h-9 items-center gap-1 rounded-xl bg-grade px-4 text-lg font-semibold text-gradeText"
 				>
 					Przejdź <FaAngleRight />
 				</Link>
