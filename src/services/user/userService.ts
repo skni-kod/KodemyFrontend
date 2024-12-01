6b12c36e4b0037178de509a624fe05316d3c4ab3
Krystian Kiełbasa <krystian.kielbasa@hotmail.com>
2024-12-01 17:51:01 +0100

    refactor: #142 hide .env file, update gitignore

--- src/services/user/userService.ts
+++ src/services/user/userService.ts
@@ -3,10 +3,11 @@ import kodemyApi from '@/utils/api';
 import { InternalServerErrorApiError } from '@/utils/api/types/apiError';
 import { User, UserFiltersParam, UserSearch, UserSortField } from '@/services/user/types';
 import { Session } from '@/contexts/SessionContext';
+import UserAuthorized from '@/services/user/types/userAuthorized';
 
 export default class UserService extends ApiService {
 	public static async getUsers(
-		params: SearchRequestParams<UserSortField, UserFiltersParam>,
+		params: SearchRequestParams<UserSortField, UserFiltersParam>
 	): Promise<Pageable<UserSearch>> {
 		const { size, page, sort, sort_direction, filters } = params;
 
@@ -15,7 +16,7 @@ export default class UserService extends ApiService {
 			page: page > 0 ? (page - 1).toString() : '0',
 			sort: UserSortField[sort],
 			sort_direction: SortDirection[sort_direction],
-			filters: filters ? JSON.stringify(filters) : '{}',
+			filters: filters ? JSON.stringify(filters) : '{}'
 		});
 
 		try {
@@ -30,6 +31,16 @@ export default class UserService extends ApiService {
 		try {
 			return await kodemyApi
 				.get<User>(`/api/users/${id}`, session ? this.withCredentials(session.token.bearer) : {})
+				.then((res) => res.data);
+		} catch (err) {
+			return Promise.reject(new InternalServerErrorApiError());
+		}
+	}
+
+	public static async getMe(): Promise<UserAuthorized> {
+		try {
+			return await kodemyApi
+				.get<UserAuthorized>(`/api/users/me`)
 				.then((res) => res.data);
 		} catch (err) {
 			return Promise.reject(new InternalServerErrorApiError());
