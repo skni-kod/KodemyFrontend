b2fc20696a9658065fdea41a4ea3e883b7b70b31
Krystian Kiełbasa <krystian.kielbasa@hotmail.com>
2024-12-03 18:06:55 +0100

    fix: #142 change logout url

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,19 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { HttpStatusCode } from 'axios';
-import { NextApiResponse } from 'next';
 import AuthService from '@/services/auth/authService';
 
 const ACCESS_TOKEN_COOKIE = 'AUTH_CONTEXT';
 const REFRESH_TOKEN_COOKIE = 'AUTH_PERSIST';
 
-export async function GET(req: NextRequest) {
-	const baseUrl = req.nextUrl.origin;
-
+export function GET(req: NextRequest) {
 	const headers: HeadersInit = {
 		'Set-Cookie': `${ACCESS_TOKEN_COOKIE}=; Path=/; Max-Age=0;, ${REFRESH_TOKEN_COOKIE}=; Path=/; Max-Age=0;`,
 	};
 
-	await AuthService.logout();
+	AuthService.logout();
 
-	return NextResponse.redirect(baseUrl, { status: HttpStatusCode.PermanentRedirect, headers });
+	return NextResponse.rewrite(req.nextUrl.origin, { status: HttpStatusCode.PermanentRedirect, headers });
 }
