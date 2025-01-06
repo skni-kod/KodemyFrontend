b186ecd9accaa00a21319763e2784eccd9659c11
Paweł Buczek <pbuczek7@wp.pl>
2025-01-06 15:43:04 +0100

    feat: #156 added animation on home button hover

--- src/components/home/HomeMoreButton.tsx
+++ src/components/home/HomeMoreButton.tsx
@@ -4,8 +4,9 @@ import { TEXT } from '@/utils/constant';
 
 export default function HomeMoreButton() {
 	return (
-		<div className="cursor-pointer flex-nowrap">
+		<div className="group relative w-fit cursor-pointer flex-nowrap">
 			<div className="font-semibold tracking-widest text-primary">{TEXT.CHECK_UPPER}</div>
+			<div className="absolute bottom-0 left-0 right-0 h-0.5 origin-center scale-x-0 bg-primary transition-all duration-500 group-hover:scale-x-100"></div>
 		</div>
 	);
 }
